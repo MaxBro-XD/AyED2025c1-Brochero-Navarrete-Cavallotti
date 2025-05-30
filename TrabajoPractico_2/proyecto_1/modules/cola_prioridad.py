3cbd1646f54e1ad98c0f5ea776f9bcebe51a5778
Caterina233 <caterinavarrete1004@gmail.com>
2025-05-30 12:56:13 -0300

    Cambio de cola de prioridad y agregado de monticulo binario

--- TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
+++ TrabajoPractico_2/proyecto_1/modules/cola_prioridad.py
@@ -1,31 +1,29 @@
-
-
-class Nodo:
-    def __init__(self, dato, prioridad, orden_llegada):
-        self.dato = dato
-        self.prioridad = (prioridad, orden_llegada)  # tupla para desempate
+from modules.paciente import Paciente
+from modules.monticulo_binario import MonticuloBinario
 
 class ColaConPrioridad:
     def __init__(self):
-        self.elementos = []
-        self.contador = 0  # orden de llegada
+        self.monticulo = MonticuloBinario()
+        self.contador = 0  # Para desempatar por orden de llegada
 
     def append(self, dato):
-        # El objeto `dato` es un paciente, usamos su riesgo como prioridad
         riesgo = dato.get_riesgo()
-        nodo = Nodo(dato, riesgo, self.contador)
+        # Insertamos directamente la tupla en el montículo
+        self.monticulo.insertar((riesgo, self.contador, dato))
         self.contador += 1
-        self.elementos.append(nodo)
-        self.elementos.sort(key=lambda x: x.prioridad)  # menor riesgo y más temprano primero
 
-    def pop(self, index=0):
-        return self.elementos.pop(index).dato
+    def pop(self):
+        # Sacamos el de mayor prioridad (menor riesgo y más antiguo)
+        _, _, dato = self.monticulo.eliminarMin()
+        return dato
 
     def __len__(self):
-        return len(self.elementos)
+        return self.monticulo.tamanoActual
 
     def __iter__(self):
-        return (nodo.dato for nodo in self.elementos)
-
-    def __getitem__(self, index):
-        return self.elementos[index].dato
+        # Iterador sin destruir el montículo original
+        copia = MonticuloBinario()
+        copia.construirMonticulo(self.monticulo.listaMonticulo[1:])
+        while copia.tamanoActual > 0:
+            _, _, dato = copia.eliminarMin()
+            yield dato
