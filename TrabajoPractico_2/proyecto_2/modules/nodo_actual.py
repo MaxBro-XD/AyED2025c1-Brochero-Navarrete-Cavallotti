dfb41dfb8255f830637d5090c46fdb153e09483f
RocioArener <arenerrocio@gmail.com>
2025-06-06 12:41:00 -0300

    Creacion del modulo nodo actual

---
+++ TrabajoPractico_2/proyecto_2/modules/nodo_actual.py
@@ -0,0 +1,10 @@
+class nodoActual:
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+
+    def __str__(self):
+        return str(self.valor)
+
+    def __repr__(self):
+        return str(self.valor)
