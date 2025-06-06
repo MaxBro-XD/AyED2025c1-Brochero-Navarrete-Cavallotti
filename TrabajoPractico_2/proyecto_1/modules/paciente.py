4a93ab616e4b38f4c667baf49a4ac93e200b9979
RocioArener <arenerrocio@gmail.com>
2025-06-06 11:59:21 -0300

    Modificacion del modulo paciente

--- TrabajoPractico_2/proyecto_1/modules/paciente.py
+++ TrabajoPractico_2/proyecto_1/modules/paciente.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from random import randint, choices
+import datetime
+import time
 
 nombres = ['Leandro', 'Mariela', 'Gastón', 'Andrea', 'Antonio', 'Estela', 'Jorge', 'Agustina']
 apellidos = ['Perez', 'Colman', 'Rodriguez', 'Juarez', 'García', 'Belgrano', 'Mendez', 'Lopez']
@@ -17,6 +19,7 @@ class Paciente:
         self.__apellido = apellidos[randint(0, n-1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
         self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
+        self.__fecha= datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def get_nombre(self):
         return self.__nombre
@@ -30,11 +33,26 @@ class Paciente:
     def get_descripcion_riesgo(self):
         return self.__descripcion
     
+    def get_fecha(self):
+        return self.__fecha
+    
     def __str__(self):
         cad = self.__nombre + ' '
         cad += self.__apellido + '\t -> '
         cad += str(self.__riesgo) + '-' + self.__descripcion
+        cad += '-Fecha de ingreso: (' + self.__fecha + ')'
         return cad
+
+
+
+  
+if __name__ == "__main__":
+    for p in range(5):
+        paciente = Paciente()
+        time.sleep(1)
+        print(paciente)
+
+
         
         
         
